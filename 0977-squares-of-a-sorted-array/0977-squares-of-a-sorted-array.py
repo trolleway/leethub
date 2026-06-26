1b350d9cb26676d55b5e42cce2b1767070538076
Artem Svetlov <trolleway@yandex.ru>
2026-06-26 18:12:52 +0300

    Time: 18 ms (10.89%), Space: 21 MB (50.75%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -1,8 +1,36 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        # trivial
-        res=list()
-        for n in nums:
-            res.append(n**2)
-        res.sort()
-        return res
+        # two pointers
+        out=[None] * len(nums)
+        m1=0
+        m2=len(nums)-1
+        counter=len(nums)-1
+        while m1 <= m2:
+            
+            if abs(nums[m1])>=abs(nums[m2]):
+                out[counter]=nums[m1]**2
+                m1+=1
+                counter = counter-1
+            else:
+                out[counter]=nums[m2]**2
+                m2-=1
+                counter = counter-1
+
+            if m1==m2:
+                stop=True
+                out[counter]=nums[m1]**2
+            
+        return out
+
+        """
+-4,-1,0,3,10
+^         ^
+
+отрицательное и положительное число после операции одинаковы
+маркер1 на первый элемент
+маркер2 на последний элемент
+цикл
+обработка большего значения под маркерами, результат поместить в конец
+сдвинуть маркер с большим значением
+
+        """
